2644e77cf1361ebffc6bac6377ceeb34806d6212
Raymond Chen <raymondc@microsoft.com>
2015-08-30 17:50:35 -0700

    Can now autonavigate based on hash passed to page

--- UI.js
+++ UI.js
@@ -1,91 +1,111 @@
-﻿function initNode(rootElement, contents, initialToggle) {
+﻿function ApiNode(rootElement, contents) {
+    this.contents = contents;
+    this.element = rootElement;
+    this.populated = false;
+    this.list = null;
 
-    var populated;
-    var list;
-
-    function populateNode() {
-        if (contents.children) {
-            list = document.createElement("ul");
-            list.style.display = "none";
-            rootElement.appendChild(list);
-            for (var name in contents.children) {
-                var item = document.createElement("li");
-                item.innerText = name;
-                initNode(item, contents.children[name]);
-                list.appendChild(item);
-            }
-        } else if (contents.members) {
-            list = document.createElement("ul");
-            list.style.display = "none";
-            rootElement.appendChild(list);
-            for (var name in contents.members) {
-                var item = document.createElement("li");
-                item.innerText = name + " (" + contents.members[name].memberKind + ")";
-                initMember(item, contents.members[name]);
-                list.appendChild(item);
-            }
-        }
-    }
-
-    function toggleNode(e) {
-        if (e.srcElement != rootElement) return;
-
-        if (!populated) {
-            populated = true;
-            populateNode();
-        }
-        if (list) {
-            if (list.style.display == "none") {
-                list.style.display = "block";
-            } else {
-                list.style.display = "none";
-            }
-        }
-        e.cancelBubble = true;
-    }
-
-    rootElement.addEventListener("click", toggleNode);
-    if (initialToggle) {
-        toggleNode({ srcElement: rootElement });
+    if (contents) {
+        contents.node = this;
+        rootElement.addEventListener("click", ApiNode.prototype.toggleNodeHandler.bind(this));
     }
 }
 
-function initMember(rootElement, contents) {
-    var populated;
-    var list;
-
-    function populateNode() {
-        list = document.createElement("ul");
-        list.style.display = "none";
-        rootElement.appendChild(list);
-        contents.files.forEach(function (fileIndex) {
-            var entry = fileTable[fileIndex];
-            var file = dirTable[entry[0]] + "/" + entry[1];
-            var item = document.createElement("li");
-            var anchor = document.createElement("a");
-            anchor.innerText =file;
-            anchor.href = "https://github.com/Microsoft/Windows-universal-samples/blob/v1.0.4/Samples/" + file;
-            anchor.target = "_blank";
-            item.appendChild(anchor);
-            list.appendChild(item);
-        });
+ApiNode.prototype.ensurePopulated = function ensurePopulated() {
+    if (!this.populated) {
+        this.populated = true;
+        this.populateNode();
     }
+};
 
-    function toggleNode(e) {
-        if (e.srcElement != rootElement) return;
+ApiNode.prototype.openNode = function openNode() {
+    this.ensurePopulated();
+    this.list.style.display = "block";
+};
 
-        if (!populated) {
-            populated = true;
-            populateNode();
+ApiNode.prototype.toggleNode = function toggleNode() {
+    this.ensurePopulated();
+    if (this.list) {
+        if (this.list.style.display == "none") {
+            this.list.style.display = "block";
+        } else {
+            this.list.style.display = "none";
         }
-        if (list) {
-            if (list.style.display == "none") {
-                list.style.display = "block";
-            } else {
-                list.style.display = "none";
+    }
+};
+
+ApiNode.prototype.toggleNodeHandler = function toggleNodeHandler(e) {
+    if (e.srcElement != this.element) return;
+    this.toggleNode();
+    e.cancelBubble = true;
+};
+
+function ContainerNode(rootElement, contents) {
+    ApiNode.call(this, rootElement, contents);
+}
+
+ContainerNode.prototype = new ApiNode;
+
+ContainerNode.prototype.populateNode = function populateNode() {
+    if (this.contents.children) {
+        this.list = document.createElement("ul");
+        this.list.style.display = "none";
+        this.element.appendChild(this.list);
+        for (var name in this.contents.children) {
+            var item = document.createElement("li");
+            item.innerText = name;
+            new ContainerNode(item, this.contents.children[name]);
+            this.list.appendChild(item);
+        }
+    } else if (this.contents.members) {
+        this.list = document.createElement("ul");
+        this.list.style.display = "none";
+        this.element.appendChild(this.list);
+        for (var name in this.contents.members) {
+            var item = document.createElement("li");
+            item.innerText = name + " (" + this.contents.members[name].memberKind + ")";
+            new MemberNode(item, this.contents.members[name]);
+            this.list.appendChild(item);
+        }
+    }
+};
+
+function MemberNode(rootElement, contents) {
+    ApiNode.call(this, rootElement, contents);
+};
+
+MemberNode.prototype = new ApiNode;
+
+MemberNode.prototype.populateNode = function populateNode() {
+    this.list = document.createElement("ul");
+    this.list.style.display = "none";
+    this.element.appendChild(this.list);
+    this.contents.files.forEach(function (fileIndex) {
+        var entry = fileTable[fileIndex];
+        var file = dirTable[entry[0]] + "/" + entry[1];
+        var item = document.createElement("li");
+        var anchor = document.createElement("a");
+        anchor.innerText =file;
+        anchor.href = "https://github.com/Microsoft/Windows-universal-samples/blob/v1.0.4/Samples/" + file;
+        anchor.target = "_blank";
+        item.appendChild(anchor);
+        this.list.appendChild(item);
+    }, this);
+};
+
+function autoNavigate(target) {
+    var pieces = target.split(".");
+    var current = contents;
+    contents.node.openNode();
+    pieces.forEach(function (name) {
+        if (current) {
+            if (current.children) {
+                current = current.children[name];
+            } else if (current.members) {
+                current = current.members[name];
             }
         }
-        e.cancelBubble = true;
-    }
-    rootElement.addEventListener("click", toggleNode);
+        if (current) {
+            current.node.openNode();
+        }
+    }, this);
 }
