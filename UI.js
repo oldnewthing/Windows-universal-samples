95fc60bbcf78ec8866b86c7f85ea1e180bd3ad2e
Raymond Chen <raymondc@microsoft.com>
2015-10-21 22:40:40 -0700

    1.0.9

--- UI.js
+++ UI.js
@@ -100,7 +100,7 @@ MemberNode.prototype.populateNode = function populateNode() {
         var entry = fileTable[fileEntry[0]];
         var file = dirTable[entry[0]] + "/" + entry[1];
         var item = document.createElement("li");
-        var href = "https://github.com/Microsoft/Windows-universal-samples/blob/v1.0.8/Samples/" + file;
+        var href = "https://github.com/Microsoft/Windows-universal-samples/blob/v1.0.9/Samples/" + file;
         item.appendChild(createAnchor(href, file));
         fileEntry[1].forEach(function (lineNumber) {
             item.appendChild(document.createTextNode(" "));
